e94721dcab0ef875bedb5e2f9a2588e65df0161c
AndrewXu23 <105327407+AndrewXu23@users.noreply.github.com>
2022-05-11 12:45:08 -0700

    Add files via upload

---
+++ test/BookTest.java
@@ -0,0 +1,37 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * A JUnit test class for the Person class
+ */
+public class BookTest {
+
+    private Book Shame;
+    private Book AmericanGods;
+
+    @Before
+    public void setUp() {
+        this.Shame = new Person("Shame", "Doe", 36);
+        this.AmericanGods = new Person("AmericanGods", "Bill", 99);
+    }
+
+    @Test
+    public void testTitle() {
+        assertEquals("Shame", this.Shame.getTitle());
+        assertEquals("AmericanGods", this.AmericanGods.getTitle());
+    }
+
+    @Test
+    public void testPrice() {
+        assertEquals(36, this.Shame.getPrice());
+        assertEquals(99, this.AmericanGods.getPrice());
+    }
+
+    @Test
+    public void testAuthor() {
+        assertEquals("Doe", this.Shame.getAuthor());
+        assertEquals("bILL", this.AmericanGods.getAuthor());
+    }
+}
