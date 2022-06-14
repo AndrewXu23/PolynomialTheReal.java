9e7b9e36d4201749b46b857e6a8df2e9c4533baf
AndrewXu23 <zxu232@gmail.com>
2022-06-14 14:36:44 -0700

    Polynormial

---
+++ Polynomial/src/Constant.java
@@ -0,0 +1,92 @@
+public class Constant implements PolyInterface{
+    private double coefficient;
+    private int power;
+
+    public Constant (double coefficient){
+        this.coefficient = coefficient;
+        this.power = 0;
+    }
+
+    /**
+     * Return the degree of the polynomial
+     * @return
+     */
+    public int getDegree(){
+        return this.power;
+    }
+
+    /**
+     * Return the coefficient for the term with specific power
+     *
+     * @return
+     */
+    public double getCoefficient(int power) {
+        if (power > 0) {
+            return 0;
+        } else {
+            return this.coefficient;
+        }
+    }
+        /**
+         * Return true: the number taken is a root
+         *        false: the number taken is not a root
+         * @return
+         */
+        public boolean isRoot(double number){
+            return false;
+        }
+
+        /**
+         * Return true: two polynomials are the same class and are qeuivalent
+         *        false: two polynomials are not the same class or not qeuivalent
+         * @return
+         */
+        public boolean isEqualTo(PolyInterface other){
+            Constant otherConstant = (Constant) other;
+            return this.coefficient == otherConstant.coefficient;
+        }
+
+        public double evaluateAt(double x){
+            return this.coefficient;
+        }
+
+        /**
+         * Return the power of the leading coefficient
+         * @return
+         */
+        public double getLeadingCoefficient(){
+            return this.coefficient;
+        }
+
+        /**
+         * Return the polynomial evaluated at 0
+         * @return
+         */
+        public double getYIntercept(){
+            return this.coefficient;
+        }
+
+        /**
+         * Return the sum of two polynomials
+         * @return
+         */
+        public double plus(PolyInterface other){
+            Constant otherConstant = (Constant) other;
+            return this.coefficient + otherConstant.coefficient;
+        }
+
+        /**
+         * returns a String representation of the polynomial
+         * @return
+         */
+        @Override
+        public String getString(){
+            return "p(x) = " +  this.coefficient;
+        }
+
+        @Override
+        public String toString(){
+            return getString();
+        }
+    }
+
